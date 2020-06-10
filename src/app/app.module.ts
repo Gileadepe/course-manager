6484ee81d3233a33342a2730b105dcd4d60ab060
Gileadepe <gil_eade@hotmail.com>
2020-06-09 21:49:29 -0300

    subir o projeto do curso de introdução ao angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { Error404Component } from './error-404/error-404.component';
+import { CourseModule } from './courses/course.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    Error404Component
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+     
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
